0199eea4620d866a3801a10c2be55a91c93a98c3
Alberto Bolanos <117311800+aibolanos@users.noreply.github.com>
2024-02-15 15:45:24 -0800

    Removed Comments and updated logic to ps1c.py

--- ps1/ps1c.py
+++ ps1/ps1c.py
@@ -1,8 +1,3 @@
-# portion_saved = input("Enter the percent of your salary to save, as a decimal: ")
-# portion_saved = float(portion_saved) #str -> float
-
-
-
 #Get user input, ask for needed info
 starting_salary = input("Enter the starting salary: ")
 starting_salary = int(starting_salary) #str -> int
@@ -12,33 +7,22 @@ semi_annual_raise = 0.07
 r = 0.04
 portion_down_payment = 0.25
 total_cost = 1000000
+epsilon = 100 #Our Savings can be in the range of $100 of Down Payment
 
 #Math
 down_payment = portion_down_payment * total_cost
-#time is 36 months
-#looking for an integer SAVINGS rate>
 
 #Savings
 def savings(starting_salary, portion_saved):
-    # print("Salary: " + str(starting_salary))
-    # print("Portion Saved: " + str(portion_saved))
     current_savings = 0.0
-    # print("Current Savings: " + str(current_savings))
     months = 0
-    # print("Months: " + str(months))
-    # print("START LOOP")
     for x in range(36):
         current_savings += current_savings * (r / 12)
-        # print("IL Current Savings return: " + str(x) + " " + str(current_savings))
         monthly_salary = starting_salary / 12
-        # print("IL Monthly Salary: " + str(x) + " " +str(monthly_salary))
         current_savings += monthly_salary * (portion_saved / 10000)
-        # print("IL Current Savings New Month: " + str(x) + " " + str(current_savings))
-        # print(" Ending Month " + str(months) + " with " + str(current_savings) + " savings and " + str(monthly_salary))
         months+=1
         if months % 6 == 0:
             starting_salary += starting_salary * semi_annual_raise
-    # print("END LOOP")
     return current_savings
 
 #Bisection Search
@@ -47,15 +31,10 @@ high = 10000
 low = 0
 guess = (high + low) / 2
 
-while savings(starting_salary, guess) > down_payment + 100 or savings(starting_salary, guess) < down_payment - 100:
-    print("High: " + str(high)+ " Low: " + str(low) + " Guess: " + str(guess/10000))
-    # print("Savings: " + str(savings(starting_salary, guess)))
-    if savings(starting_salary, guess) < down_payment - 100:
-        #print(savings(starting_salary, guess) < down_payment - 100)
-        print("Too Low!")
+while abs(down_payment - savings(starting_salary, guess)) > epsilon:
+    if down_payment - savings(starting_salary, guess) > epsilon:
         low = guess
     else:
-        print("Too High!")
         high = guess
     guess = (high+low)/2
     steps+=1
