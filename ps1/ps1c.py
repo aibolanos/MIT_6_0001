0a2ebeccd107ccbacf4fd3d9779c6e0ea4bb20fc
Alberto Bolanos <117311800+aibolanos@users.noreply.github.com>
2024-02-15 16:09:32 -0800

    Found out my algorithm is wrong, keep working

--- ps1/ps1c.py
+++ ps1/ps1c.py
@@ -11,18 +11,20 @@ epsilon = 100 #Our Savings can be in the range of $100 of Down Payment
 
 #Math
 down_payment = portion_down_payment * total_cost
+print(down_payment)
 
 #Savings
 def savings(starting_salary, portion_saved):
-    current_savings = 0.0
+    current_savings = 0
     months = 0
+    monthly_salary = starting_salary / 12
     for x in range(36):
         current_savings += current_savings * (r / 12)
-        monthly_salary = starting_salary / 12
         current_savings += monthly_salary * (portion_saved / 10000)
         months+=1
         if months % 6 == 0:
             starting_salary += starting_salary * semi_annual_raise
+            monthly_salary = starting_salary / 12
     return current_savings
 
 #Bisection Search
@@ -32,13 +34,14 @@ low = 0
 guess = (high + low) / 2
 
 while abs(down_payment - savings(starting_salary, guess)) > epsilon:
+    print(abs(down_payment - savings(starting_salary, guess)))
     if down_payment - savings(starting_salary, guess) > epsilon:
         low = guess
     else:
         high = guess
     guess = (high+low)/2
     steps+=1
-
+print(guess)
 #return
 print("Best savings rate: " + str(round(guess/10000, 4)))
 print("Steps in bisection search: " + str(steps))
