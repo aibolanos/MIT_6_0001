035e723c46f5705392903da5f46687d1a4e7f3b9
Alberto Bolanos <a.bolanos444@gmail.com>
2024-03-24 14:11:34 -0700

    Adding test cases:

--- ps4/ps4c.py
+++ ps4/ps4c.py
@@ -184,3 +184,12 @@ if __name__ == '__main__':
     print("Decrypted message:", enc_message.decrypt_message())
      
     #TODO: WRITE YOUR TEST CASES HERE
+
+    message = SubMessage("Uh, oh. This is a test case.")
+    permutation = "uoaie"
+    enc_dict = message.build_transpose_dict(permutation)
+    print("Original message:", message.get_message_text(), "Permutation:", permutation)
+    print("Expected encryption:", "Ah, eh. Thos os i tust cisu.")
+    print("Actual encryption:", message.apply_transpose(enc_dict))
+    enc_message = EncryptedSubMessage(message.apply_transpose(enc_dict))
+    print("Decrypted message:", enc_message.decrypt_message())
