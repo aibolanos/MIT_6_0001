01a7e8fefbe07f9c8ce73e708fdb86d9ce2b88ac
Alberto Bolanos <a.bolanos444@gmail.com>
2024-03-24 14:18:53 -0700

    ADDED MORE TESTS

--- ps4/ps4c.py
+++ ps4/ps4c.py
@@ -193,3 +193,12 @@ if __name__ == '__main__':
     print("Actual encryption:", message.apply_transpose(enc_dict))
     enc_message = EncryptedSubMessage(message.apply_transpose(enc_dict))
     print("Decrypted message:", enc_message.decrypt_message())
+
+    message = SubMessage("Uh, oh. This is a test case.")
+    permutation = "aeiou"
+    enc_dict = message.build_transpose_dict(permutation)
+    print("Original message:", message.get_message_text(), "Permutation:", permutation)
+    print("Expected encryption:", "Uh, oh. This is a test case.")
+    print("Actual encryption:", message.apply_transpose(enc_dict))
+    enc_message = EncryptedSubMessage(message.apply_transpose(enc_dict))
+    print("Decrypted message:", enc_message.decrypt_message())
